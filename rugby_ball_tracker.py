c9239d2eed8cf1a3b719cf75e8a987c4fc003111
rjr100 <75345520+rjr100@users.noreply.github.com>
2022-06-01 20:58:25 +1200

    Add files via upload

--- rugby_ball_tracker.py
+++ rugby_ball_tracker.py
@@ -3,6 +3,7 @@ import cv2 as cv
 import time
 import argparse
 from tracker import *
+import matplotlib.pyplot as plt
 import numpy as np
 from filterpy.kalman import KalmanFilter, UnscentedKalmanFilter, MerweScaledSigmaPoints
 from skimage import data, color
@@ -10,62 +11,100 @@ from skimage.transform import hough_circle, hough_circle_peaks, hough_ellipse
 from skimage.feature import canny
 from skimage.draw import circle_perimeter
 from skimage.util import img_as_ubyte
-"""
-Questions for the homies
-1. How can I only use the maximum found contour
-2. Fix my Kalman filter pls
-3. Display Kalman filter stuff
-4. Display tragectory
-5. How to tell if the shape is elliptical? Can I iterate through the shape on the mask and see if
-it is mostly white?
+from scipy.optimize import curve_fit
 
-
-Compare 
-- Using median filter vs using open/close
-- Using ellipse vs using Centroids
-- Using Kalman filtering vs not to see if the graph is more accurate
-
-
-
-Report sections:
-- Mask
-- Detection
-- Evaluation metrics
-"""
-
+# MACROS
 wait_time = 10
-
-#parser = argparse.ArgumentParser(description='This program shows how to use background subtraction methods provided by \
-                                              #OpenCV. You can process both videos and images.')
-#parser.add_argument('--input', type=str, help='Path to a video or a sequence of image.', default=-1)
-#parser.add_argument('--algo', type=str, help='Background subtraction method (KNN, MOG2).', default='MOG2')
-#args = parser.parse_args()
 
 #create tracker object
 tracker = EuclideanDistTracker()
 
-backSub1 = cv.createBackgroundSubtractorMOG2(history=10, varThreshold=5,detectShadows=False)
-files = ['./Footage/lineout1.mp4','./Footage/players.mp4','./Footage/test4.mp4', './Footage/test2.mp4','./Footage/test4.mp4', './Footage/test6.mp4', -1]
+backSub1 = cv.createBackgroundSubtractorMOG2(history=10, varThreshold=20,detectShadows=False)
+files = ['./Footage/COSC_Footage.mp4','./Footage/players2.mp4','./Footage/one_player.mp4','./Footage/COSC_Indoor_Dim.mp4']
+#files = ['./Footage/players2.mp4','./Footage/one_player.mp4']
 
+def model_f(x, a, b, c):
+    return a*(x-b)**2 + c
 
+def average(array):
+    return sum(array)/len(array)
 
-def find_ball_ellipse(frame, distortion):
+def fit_parabola(ball_pos, image, h):
+    try:
+        ball_pos_buf = ball_pos
+        for pos in ball_pos:
+            pos[1] = h - pos[1]
+        dt = np.array(ball_pos)
+        
+        # Preparing X and y data from the given data
+        X = dt[:, 0]
+        y = dt[:, 1]
+        
+        # Calculating parameters (Here, intercept-theta1 and slope-theta0)
+        # of the line using the numpy.polyfit() function
+        popt, pcov = curve_fit(model_f, X, y, p0=[1,1,1])
+        fig, ax = plt.subplots()
+        
+        a_opt, b_opt, c_opt = popt
+        x_model = np.linspace(min(X), max(X), 100)
+        y_model = model_f(x_model, a_opt, b_opt, c_opt)
+        errors_sq = []
+        
+        for pos in ball_pos_buf:
+            errors_sq.append((a_opt*(pos[0]-b_opt)**2 + c_opt - pos[1])**2) 
+        ax.set_xlim(0,640)
+        ax.set_ylim(0,480)
+        ax.scatter(X,y)
+        ax.plot(x_model,y_model, color='r')
+        plt.show()
+    except:
+        print("Parameters not found")
+        return
+
+    while(1):
+        keyboard = cv.waitKey()
+        if keyboard & 0xFF == ord('w'):
+            plt.close()
+            return
+    #except:
+        #print('Couldnt fit a curve')
+
+def create_mask(frame):
+    start_time = time.time()
+    fgMask1 = backSub1.apply(frame)
+    
+    median = cv.medianBlur(fgMask1, 3)     # doing this twice does a better job of removing salt & pepper noise
+    #median2 = cv.medianBlur(median, 5)     # doing this twice does a better job of removing salt & pepper noise
+    ##opened = cv.morphologyEx(fgMask1, cv.MORPH_OPEN, np.ones((5, 5)))
+    closed = cv.morphologyEx(median, cv.MORPH_CLOSE, np.ones((15, 15)))
+    finish_time = time.time()
+    time_taken = finish_time - start_time
+    #print('mask time = ' + str(time_taken))	
+    #closed = fgMask1
+    cv.imshow('MASK', fgMask1)
+    cv.imshow('Morphology', closed)  
+    return closed, time_taken
+
+def find_ball_ellipse(frame, mask, distortion):
     height, width, _ = frame.shape
     
-    fgMask1 = backSub1.apply(frame)
+    #fgMask1 = backSub1.apply(frame)
     
     #median = cv.medianBlur(fgMask1, 9)     # doing this twice does a better job of removing salt & pepper noise
     #median2 = cv.medianBlur(median, 9)     # doing this twice does a better job of removing salt & pepper noise
-    opened = cv.morphologyEx(fgMask1, cv.MORPH_OPEN, np.ones((5, 5)))
-    closed = cv.morphologyEx(opened, cv.MORPH_CLOSE, np.ones((21, 21)))
+    ##opened = cv.morphologyEx(fgMask1, cv.MORPH_OPEN, np.ones((5, 5)))
+    #closed = cv.morphologyEx(median2, cv.MORPH_CLOSE, np.ones((21, 21)))
+    #cv.imshow('MASK', closed)
     #edges = cv.Canny(closed,100,200)
     #result = hough_ellipse(edges, threshold=250,
                        #min_size=100, max_size=120)
     #cv.imshow('Result', result)
     #cv.imshow('CANNY', edges)
     
+    start_time = time.time()
     # Object detection
-    contours, _ = cv.findContours(closed, cv.RETR_CCOMP, cv.CHAIN_APPROX_NONE)
+    contours, _ = cv.findContours(mask, cv.RETR_CCOMP, cv.CHAIN_APPROX_NONE)
+    end_time = time.time()
     #cv.imshow('Contours', contours)
     detections = []
     ellipse = []
@@ -73,17 +112,13 @@ def find_ball_ellipse(frame, distortion):
     ball_ellipse = None
     is_ball = False    
     
-    
-    #for cnt in contours:
-        #cv.drawContours(frame, cnt, 0, (255,255,0), 5)
+    start_time1 = time.time()
     if len(contours) > 0:
         sorted_contours = sorted(contours, key=cv.contourArea, reverse=True)
         i = 0
-        #cnt = max(contours, key=cv.contourArea)  
         for cnt in sorted_contours:
             area = cv.contourArea(cnt)
-            #cv.drawContours(frame, cnt, 0, (255,255,0), 5)
-            if area > 200:
+            if 2000 > area > 100:
                 ellipse = cv.fitEllipse(cnt)
                 centerX = ellipse[0][0]
                 centerY = ellipse[0][1]
@@ -94,7 +129,7 @@ def find_ball_ellipse(frame, distortion):
                
                 CD = area/ellipse_A
                 ratio = ellipse_w/ellipse_h
-                print(ratio)
+                #print(ratio)
                 if CD > 0.95:
                     pass_CD = True
                 else:
@@ -103,31 +138,24 @@ def find_ball_ellipse(frame, distortion):
                     pass_ratio = True
                 else:
                     pass_ratio = False  
-                    
+                #print("Pass CD: " + str(pass_CD) + " Pass ratio:" + str(pass_ratio))
                 if pass_CD and pass_ratio:
                     is_ball = True
                     cv.ellipse(frame, ellipse, (0, 255,0), 3) 
                     cv.putText(frame, 'Ball', [int(centerX)+10,int(centerY)+10], 0, 1, (0,0,255),2)
                     ball_ellipse = ellipse
                     position = [centerX, centerY]
+                    
                     return detections, position, ball_ellipse, is_ball
                 else:
                     is_ball = False        
-                    cv.putText(frame, 'Player', [int(centerX)+10,int(centerY)+10], 0, 1, (0,255,255),2)
+                    #cv.putText(frame, 'Player', [int(centerX)+10,int(centerY)+10], 0, 1, (0,255,255),2)
                     ball_ellipse = None
                     position = []
                 #cv.putText(frame, str(CD), [int(centerX)+10,int(centerY)+10], 0, 1, (0,0,255),2)
-                #print(ellipse)
-                #cv.ellipse(closed, ellipse, (0, 255,0), 3)
-                #cv.circle(frame, (int(centerX), int(centerY)), 5, (0,255,0), 5)    
-                
             i += 1
             if i > 4:
                 break
-    
-
-    cv.imshow('MASK', closed)
-    
     return detections, position, ball_ellipse, is_ball
 
 def track_ball(detections, frame):
@@ -137,21 +165,6 @@ def track_ball(detections, frame):
         cv.putText(frame, str(id), (x, y - 15), cv.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
         cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)    
 
-
-def fx(x, dt):
-    # state transition function - predict next state based
-    # on constant velocity model x = vt + x_0
-    F = np.array([[1, dt, 0, 0],
-                  [0, 1, 0, 0],
-                  [0, 0, 1, dt],
-                  [0, 0, 0, 1]], dtype=float)
-    return np.dot(F, x)
-
-def hx(x):
-    # measurement function - convert state into a measurement
-    # where measurements are [x_pos, y_pos]
-    return np.array([x[0], x[2]])
-
 def main(file=-1):
     cv.namedWindow("Frame", cv.WINDOW_NORMAL)
     
@@ -160,63 +173,41 @@ def main(file=-1):
     w = int(capture.get(cv.CAP_PROP_FRAME_WIDTH))
     h = int(capture.get(cv.CAP_PROP_FRAME_HEIGHT))
     print((w, h))
-    distortion = (w/640, h/480)
-    cv.resizeWindow("Frame", 640, 480) 
-    time.sleep(1)
+    
+    if (file != -1):
+        distortion = (w/640, h/480)
+        cv.resizeWindow("Frame", 640, int(h/(w/640)))
     #fps = capture.get(cv.CAP_PROP_FPS)
     fps = 1/wait_time
     dt = 1/fps
     ballpos = []
-
+    images = []
+    tracked_points = []
+    ball_count = 0
+    no_ball = 0
+    want_output = False
+    if want_output:
+        out_file = './Out_footage/output_footage' + str(time.ctime()) +'.avi'
+        out = cv.VideoWriter(out_file,cv.VideoWriter_fourcc('M','J','P','G'), 10, (640,480))
+    frame_index = 0
+    frame_saved = False
+    
     while capture.isOpened():
+        #print(frame_index)
         ret, in_frame = capture.read()
-        
-
         if in_frame is None:
             break
+        new_h = int(h/(w/640))
+        frame = cv.resize(in_frame,(640,new_h),fx=0,fy=0, interpolation = cv.INTER_CUBIC)
         
-        frame = cv.resize(in_frame,(640,480),fx=0,fy=0, interpolation = cv.INTER_CUBIC)
+        # Create Mask
+        mask, mask_time = create_mask(frame)
         # Ball detecting
-        detections, position, ellipse, is_ball = find_ball_ellipse(frame, distortion)
-        #print(ellipse)
-        # Object tracking
-        #track_ball(detections, frame)
-        #Kalman filtering
+        detections, position, ellipse, is_ball = find_ball_ellipse(frame, mask, distortion)
         
-        ##points = MerweScaledSigmaPoints(4, alpha=.1, beta=2., kappa=-1)
-        ##kalman = UnscentedKalmanFilter(dim_x=4, dim_z=2, dt=dt, fx=fx, hx=hx, points=points)
-        ##kalman.x = np.array([1,1,1,1])  # Initial State
-        ##kalman.P = np.array([[2,0,0,0],
-                             ##[0,2,0,0],
-                             ##[0,0,2,0],
-                             ##[0,0,0,2]], np.float32)  # Covariance Matrix
-        ##kalman.R = np.array([[1,0],
-                             ##[0,1]], np.float32)  # Measurement Noise
-        ##kalman.Q = np.array([[1,0,0,0],
-                             ##[0,1,0,0],
-                             ##[0,0,100,0],
-                             ##[0,0,0,100]], np.float32)  # Process Noise=2)
-        
-        ##kalman.predict()  # Predict the ball's position.
-        ##center_predict = (int(kalman.x[0]), int(kalman.x[2]))
-        
-        #if len(position) > 0:
-            ## The Kalman filter expects the x,y coordinates in a 2D array.
-            #measured = np.array([position[0], position[1]], dtype="float32")
-            ## Update the Kalman filter with the current ball location if we have it.
-            #kalman.update(measured)
-
-   
-        ## Draw an ellipse showing the uncertainty of the predicted position.
-        #center = (int(kalman.x[0]), int(kalman.x[2]))
-        ##print(center[0] - center_predict[0], center[1] - center_predict[1])
-        ##print(int(kalman.x[0]), int(kalman.x[1]))
-        #axis_lengths = (int(kalman.P_prior[0, 0]), int(kalman.P_prior[1, 1]))
-        
-        
-        ##print(position)
+  
         if(position != []):
-            if len(ballpos) < 10:
+            if len(ballpos) < 15:
                 if position[0] > 0 and position[1] > 0:
                     ballpos.append(position)
                     #print(ballpos)
@@ -224,24 +215,48 @@ def main(file=-1):
                 if position[0] > 0 and position[1] > 0:
                     ballpos.pop(0)
                     ballpos.append(position)
-                    #print(ballpos)                
+                    #print(ballpos)        
                     
+        # Mark Ball locations with green circles
         if len(ballpos) > 1:
             for pos in ballpos:
                 x = pos[0]
                 y = pos[1]
-                cv.circle(frame, (int(x),int(y)), 2, (0,255,0), 5)
+                cv.circle(frame, (int(x),int(y)), 1, (0,255,0), 3)
         
-        #if ellipse != []:
-            #cv.ellipse(frame, (center, ellipse[1], ellipse[2]), (255, 0, 0), 3) #blue
-            ##cv.ellipse(frame, (center_predict, ellipse[1], ellipse[2]), (0, 0, 255), 3) #red
-        
+        # If you get 5 successful ball readings, fit a parabola
+        if is_ball == True:
+            ball_count += 1
+            no_ball = 0
+            #print(ball_count)
+
+            if ball_count >= 6 and not frame_saved:
+                #images.append(frame)
+                #tracked_points.append(ballpos)
+                
+                frame_saved = True
+        else:
+            no_ball += 1
+            if no_ball > 2:
+                if frame_saved:
+                    #print(ballpos)
+                    frame_saved = False
+                    fit_parabola(ballpos, frame, new_h)                    
+                ball_count = 0
+                ballpos = []
+
+        if want_output:
+            out.write(mask)
         cv.imshow('Frame', frame)
-        #cv.imshow('MOG2', median2)   
+        frame_index+=1       
+        
         keyboard = cv.waitKey()
         if keyboard & 0xFF == ord('q'):
             break
+    
     capture.release()
+    if want_output:
+        out.release()   
 
 if __name__ == "__main__":
     #main()
